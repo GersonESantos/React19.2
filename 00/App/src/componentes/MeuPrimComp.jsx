ac352679bd3c4c435a8ec1e4838f20093b912517
GersonESS <gebhsantos@gmail.com>
2026-02-16 13:10:57 -0300

    q

---
+++ 00/App/src/componentes/MeuPrimComp.jsx
@@ -0,0 +1,10 @@
+function MeuPrimComp() {
+    return (
+        <div>
+            <h1>Meu Primeiro Componente</h1>
+            <p>Este é o meu primeiro componente React!</p>
+        </div>
+    );
+}
+
+export default MeuPrimComp;
