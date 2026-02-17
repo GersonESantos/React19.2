493c01aa439fc2f089b63dddff1451161ef54690
GersonESS <gebhsantos@gmail.com>
2026-02-17 05:56:55 -0300

    l

---
+++ Raulf/01/App/src/main.jsx
@@ -0,0 +1,8 @@
+import {createRoot} from 'react-dom/client';
+import MeuPrimComp from './componentes/MeuPrimComp';
+
+createRoot(document.getElementById('root')).render(
+  <MeuPrimComp />
+);
+
+//import MeuPrimComp from './componentes/MeuPrimComp';
